36f1d993e39a58150c6b83c54fd047f05eb6ea18
Reuben Victoria <reubenvickie20@gmail.com>
2023-03-29 13:53:24 +0100

    chore: tailwind extend theme set-up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["sans-serif"],
+        manrope: ["Manrope", "sans-serif"],
+      },
+      colors: {
+        background: {
+          headerBg: "#161616",
+          homeBg: "#efeadf",
+          buttonBg: "#486fff",
+        },
+        text:{
+          buttonText: "#f5f5f5"
+        }
+      },
+    },
   },
   plugins: [],
 };
