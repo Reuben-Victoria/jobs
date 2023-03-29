47e730cdcbba2d89b95ae8deb7877a8dfb1984fe
Reuben Victoria <reubenvickie20@gmail.com>
2023-03-29 13:55:20 +0100

    refactor: fix box shadow of header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 export const Header = () => {
   return (
-    <header className='p-4 px-6 fixed top-0 right-0 left-0 md:p-6 flex justify-between items-center md:px-[150px] bg-[#161616] shadow-[0 4px 8px 0]'>
+    <header className='p-4 px-6 fixed top-0 right-0 left-0 md:p-6 flex justify-between items-center md:px-[150px] bg-background-headerBg shadow-lg' >
       <div>
-        <h1 className='text-red-500 underline decoration-teal-500 font-bold text-2xl md:text-3xl'>
+        <h1 className='text-red-500 underline decoration-teal-500 font-bold text-2xl md:text-3xl cursor-pointer'>
           Logo
         </h1>
       </div>
